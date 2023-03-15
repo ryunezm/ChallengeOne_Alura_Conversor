f33f55a46f96bb4163cbf5ae6ed96a86b17a5f84
ryunezm <61928963+ryunezm@users.noreply.github.com>
2023-03-15 14:22:09 -0500

    20230315_1400: Finishing view.

--- src/main/java/com/ryunezm/apps/javacurrconv/view/mainWindow.java
+++ src/main/java/com/ryunezm/apps/javacurrconv/view/mainWindow.java
@@ -1,34 +1,93 @@
 package com.ryunezm.apps.javacurrconv.view;
 
+import com.formdev.flatlaf.*;
+import com.ryunezm.apps.javacurrconv.model.enums.Currency;
+import com.ryunezm.apps.javacurrconv.model.enums.Length;
+import com.ryunezm.apps.javacurrconv.model.enums.Measure;
+import com.ryunezm.apps.javacurrconv.model.enums.Mass;
+import com.ryunezm.apps.javacurrconv.model.enums.Temperature;
+import com.ryunezm.apps.javacurrconv.model.enums.Time;
 import javax.swing.*;
+import java.util.Objects;
 
-public class mainWindow {
+public class mainWindow extends JFrame {
 
-    private JComboBox listTop_ComboBox;
-    private JComboBox listBottom_ComboBox;
+    private JComboBox<Object> listTop_ComboBox;
+    private JComboBox<Object> listBottom_ComboBox;
     private JTextField nfTop;
     private JTextField nfBottom;
     private JCheckBox showExtendedMeasuresCheckBox;
     private JLabel converter_Label;
     private JLabel lblAuthorRyunezm;
-    private JComboBox listMeasures_ComboBox;
+    private JComboBox<Measure> listMeasures_ComboBox;
     private JPanel mainPanel;
 
-    public static void fillComboBox(String measure){
-        switch (measure){
-            case ("Human"):
-                break;
-            default:
-                System.out.println("Nothing");
+    public mainWindow(){
+        setTitle("Converter");
+        setSize(600, 250);
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        //listMeasures_ComboBox.addItem("Select a measure");
+        for (Measure measure : Measure.values()) {
+            listMeasures_ComboBox.addItem(measure);
         }
+
+        listMeasures_ComboBox.addActionListener(e -> {
+            if (listMeasures_ComboBox.getSelectedIndex()!=0 && listMeasures_ComboBox.getItemCount()==Measure.values().length){
+                listMeasures_ComboBox.removeItemAt(0);
+            }
+
+            switch ((Measure) Objects.requireNonNull(listMeasures_ComboBox.getSelectedItem())) {
+                case Currency -> {
+                    listBottom_ComboBox.removeAllItems();
+                    listTop_ComboBox.removeAllItems();
+                    for (Currency currency : Currency.values()) {
+                        listTop_ComboBox.addItem(currency.getName() + " (" + currency.getSymbol() + ")");
+                        listBottom_ComboBox.addItem(currency.getName() + " (" + currency.getSymbol() + ")");
+                    }
+                }
+                case Length -> {
+                    listBottom_ComboBox.removeAllItems();
+                    listTop_ComboBox.removeAllItems();
+                    for (Length length : Length.values()) {
+                        listTop_ComboBox.addItem(length.getName() + " (" + length.getSymbol() + ")");
+                        listBottom_ComboBox.addItem(length.getName() + " (" + length.getSymbol() + ")");
+                    }
+                }
+                case Mass -> {
+                    listBottom_ComboBox.removeAllItems();
+                    listTop_ComboBox.removeAllItems();
+                    for (Mass mass : Mass.values()) {
+                        listTop_ComboBox.addItem(mass.getName() + " (" + mass.getSymbol() + ")");
+                        listBottom_ComboBox.addItem(mass.getName() + " (" + mass.getSymbol() + ")");
+                    }
+                }
+                case Temperature -> {
+                    listBottom_ComboBox.removeAllItems();
+                    listTop_ComboBox.removeAllItems();
+                    for (Temperature temperature : Temperature.values()) {
+                        listTop_ComboBox.addItem(temperature.getName() + " (" + temperature.getSymbol() + ")");
+                        listBottom_ComboBox.addItem(temperature.getName() + " (" + temperature.getSymbol() + ")");
+                    }
+                }
+                case Time -> {
+                    listBottom_ComboBox.removeAllItems();
+                    listTop_ComboBox.removeAllItems();
+                    for (Time time : Time.values()) {
+                        listTop_ComboBox.addItem(time.getName() + " (" + time.getSymbol() + ")");
+                        listBottom_ComboBox.addItem(time.getName() + " (" + time.getSymbol() + ")");
+                    }
+                }
+                default -> {}
+            }
+        });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("mainWindow");
-        frame.setContentPane(new mainWindow().mainPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        FlatLightLaf.setup();
+        mainWindow mw = new mainWindow();
     }
 }
 
